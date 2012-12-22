924eeb972a3ad5b98feb167d48a2e1f770605e97
Lucas Tan <tbhtan3@gmail.com>
2012-12-22 22:31:30 +0800

    added thread and logging options

--- ProxyServer.java
+++ ProxyServer.java
@@ -13,31 +13,106 @@ import java.net.*;
  */
 public class ProxyServer
 {
-    private static final int NUM_THREADS = 3;
+    private static final int DEFAULT_NUM_THREADS = 3;
+    private static final boolean DEFAULT_LOGGING = true;
+
+    private static class Options
+    {
+        public int numThreads = DEFAULT_NUM_THREADS;
+        public boolean logging = DEFAULT_LOGGING;
+        public int port = -1;
+        public boolean ok = false;
+
+        public static void printUsage()
+        {
+            System.err.println(
+            "Usage: java ProxyServer <port> [options]\n"+
+            "options:\n"+
+            "-t : Number of threads\n"+
+            "-s : No logging\n"
+            );
+        }
+
+        public Options(String[] args)
+        {
+            try
+            {
+                parse(args);
+
+                if (port <= 0)
+                    throw new Exception("Please specify the listen port");
+
+                if (numThreads <= 0)
+                    throw new Exception
+                        ("Please specify positive number of threads");
+
+                ok = true;
+            }
+            catch (Exception e)
+            {
+                System.err.println("error parsing options: "+e.getMessage());
+                
+                printUsage();
+            }
+        }
+
+        private void parse(String[] args) throws Exception
+        {
+            for (int i = 0; i < args.length; i++)
+            {
+                String a = args[i];
+                String next = i != args.length - 1 ? args[i+1] : null;
+
+                if(a.startsWith("-") && a.length() >= 2)
+                {
+                    char c = a.charAt(1);
+                    switch(c)
+                    {
+                    case 't':
+                        if (next == null)
+                            throw new Exception("no. of threads not specified");
+                        numThreads = Integer.parseInt(next);
+                        i++;
+                        break;
+
+                    case 's':
+                        logging = false;
+                        break;
+                    }
+                }
+                else
+                {
+                    port = Integer.parseInt(a);
+                }
+            }
+        }
+    }
 
     public static void main(String args[])
         throws Exception
     {
-        if (args.length != 1)
-        {
-            System.err.println("Usage: java ProxyServer <port>");
-            return;
-        }
-
-        final int port = Integer.parseInt(args[0]);
+        Options opts = new Options(args);
+        if (! opts.ok) return;
 
         // 20 max connections
-        final ServerSocket sock = new ServerSocket(port, 20);
+        final ServerSocket sock = new ServerSocket(opts.port, 20);
        
         final List<ClientHandler> handlers = new ArrayList<ClientHandler>();
-        for (int i = 0; i < NUM_THREADS; i++)
+        for (int i = 0; i < opts.numThreads; i++)
         {
-            OutputStream bosClient = new BufferedOutputStream(
+            OutputStream bosClient = new NullOutputStream();
+            OutputStream bosServ = bosClient;
+
+            if (opts.logging)
+            {
+                bosClient = new BufferedOutputStream(
                                 new FileOutputStream(
                                     "proxy."+i+".client.log", true));
-            OutputStream bosServ = new BufferedOutputStream(
+                bosServ = new BufferedOutputStream(
                                 new FileOutputStream(
                                     "proxy."+i+".serv.log", true));
+            }
+
             ClientHandler h = new ClientHandler(bosClient, bosServ);
             handlers.add(h);
             h.start();
@@ -58,6 +133,7 @@ public class ProxyServer
                 remoteAddress.getCanonicalHostName() + ":" + 
                 remotePort + " (" + remoteAddress.getHostAddress() + ")");
             
+            // round robin
             ClientHandler h = handlers.get(idx);
             h.post(clientSock);
             idx = (idx + 1) % handlers.size();
