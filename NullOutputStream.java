2bf89b638b6666dd768fff9ce5b0eeaf0f2f580f
Lucas Tan <tbhtan3@gmail.com>
2012-12-22 22:31:01 +0800

    added NullOutputStream

---
+++ NullOutputStream.java
@@ -0,0 +1,50 @@
+/**
+ * @author Lucas Tan
+ */ 
+
+import java.io.*;
+
+/**
+ * An output stream that discards all data written to it.
+ */
+public class NullOutputStream extends OutputStream
+{
+    public NullOutputStream()
+    {
+        // nothing
+    }
+    
+    @Override
+    public void write(byte[]b)
+        throws IOException
+    {
+        // nothing
+    }
+
+    @Override
+    public void write(int i) throws IOException
+    {
+        // nothing
+    }
+    
+    @Override
+    public void write(byte[] b, int off, int len)
+        throws IOException
+    {
+        // nothing
+    }
+
+    @Override
+    public void close() throws IOException
+    {
+    
+        // nothing
+    }
+
+    @Override
+    public void flush() throws IOException
+    {
+        // nothing
+    }
+}
+
